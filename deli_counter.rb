b7f78a9367c6175c142ae97971472809b36c7e99
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-25 18:50:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(array)
   if array.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     array.each_with_index.map do |name, index|
       return "The line is currently: #{index+1}. #{name}"
