014ee2cc595f83df2ec19e15a7d3d63f997ab64b
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-25 19:03:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,5 @@ def line(array)
 end
 def take_a_number(array,name)
   array.push(name)
-  puts "Welcom, #{name}. You are number #{array.index(name)} in line."
+  puts "Welcom, #{name}. You are number #{array.index(name)+1} in line."
 end
