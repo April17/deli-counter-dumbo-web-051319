77f94c54a34d49ecefd2a27843fb546972c7c4c6
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-25 19:29:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,3 +19,10 @@ def take_a_number(array,name)
   return array
 end
 def now_serving (array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
