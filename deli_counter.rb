8a2685148a6526506cd9ffd463bb3e07bc5dadf2
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-25 18:51:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,12 @@
 # Write your code here.
 def line(array)
+  ary = []
   if array.length == 0
     puts "The line is currently empty."
   else
     array.each_with_index.map do |name, index|
-      puts "The line is currently: #{index+1}. #{name}"
+      ary[index] = "#{index+1}. #{name}"
     end
+    puts "The line is currently: #{ary}"
   end
 end
