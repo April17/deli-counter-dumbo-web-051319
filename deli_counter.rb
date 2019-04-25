79fbff71bdecd3803ef90ff50e3d0123e7fcc491
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-25 18:45:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,3 +2,6 @@
 def line (array)
   if !array.length
     return "The line is currently empty."
+  else
+    array.each_with_index.map do |name, index|
+      return "The line is currently: #{index+1}. #{name}"
